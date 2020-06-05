c283cb8600d2ba590a206aef36bcdc19875154b8
Noe-Louvell <noe.louvel@viacesi.fr>
2020-06-05 11:07:53 +0200

    add attempt

--- api.py
+++ api.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests, json, random
+import requests, json, random, time
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
+    attempt = []
+    begin = time.time()
     userPrice = -20
-    randNumbObject = random.randint(1, 20)
+    randNumb = random.randint(1, 20)
 
     if request.method == "POST":
+        randNumb = int(request.form["randNumb"])
         userPrice = int(request.form["priceForm"])
+        attempt = json.loads(request.form["attempt"])
+        attempt.append(userPrice)
 
     ApiKey = "49b27112-58be-47e3-b681-60cb722c0755",
     url = "https://api.cdiscount.com/OpenApi/json/Search"
@@ -22,7 +28,7 @@ def index():
             "SearchRequest": {
                 "Keyword": "bouteille",
                 "Pagination": {
-                    "ItemsPerPage": randNumbObject,
+                    "ItemsPerPage": randNumb,
                     "PageNumber": 1
                 },
                 "Filters": {
@@ -37,11 +43,12 @@ def index():
         }
     r = requests.post(url, data=json.dumps(params))
     name = (r.json()['Products'][0]['Name'])
-    price =int(float(r.json()['Products'][0]['BestOffer']['SalePrice']))
+    price = int(float(r.json()['Products'][0]['BestOffer']['SalePrice']))
     image = (r.json()['Products'][0]['MainImageUrl'])
 
-    return render_template("main.html", NAME=name, PRICE=price, IMAGE=image, USERPRICE=userPrice)
+    return render_template("main.html", NAME=name, PRICE=price, IMAGE=image, USERPRICE=userPrice, BEGIN=begin, RANDNUMB=randNumb, ATTEMPT=attempt)
 
 
 if __name__ == "__main__":
     app.run()
+    Bootstrap(app)
